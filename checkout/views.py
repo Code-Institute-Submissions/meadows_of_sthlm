c63821e391ad7bbddae89611f83fd8eb91e7da84
Sabine <sabine@21.lv>
2020-08-25 15:32:30 +0200

    added stripe payment selements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HJz80Hs8Nmiy0aZy7QCaM1iEMCfieZhcmyjRz1AHSVJHRV1MnPsYN3ndCqvlM0QqsqlMwfJsSjCJx5QyOSfhxrG00B22hJ45f',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
