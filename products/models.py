72daf1efadaf55137cb245dee69d3660dcfe5097
sabinemm <sabine@21.lv>
2020-10-06 20:56:29 +0300

    remove unnecesarry fields

--- products/models.py
+++ products/models.py
@@ -24,7 +24,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     dimensions = models.CharField(max_length=254, null=True, blank=True)
     size = models.CharField(max_length=254, null=True, blank=True)
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
